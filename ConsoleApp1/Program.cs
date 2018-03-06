ae0c08ad8bdaaeddacca87f1a7446b096f33761f
HunterAW <34281872+HunterAW@users.noreply.github.com>
2018-03-06 12:23:15 -0600

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NasaImage;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var image = Image.RequestApod();
+            image.Wait();
+
+            Console.WriteLine($"Image Request Success: {image.Result.Url}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NasaImage/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace NasaImage
+{
+    public class Image
+    {
+        private const string Address = "https://api.nasa.gov/planetary/apod?api_key=jJSnOuXvEk2vL2QFgxcuqVgWSgmtopRCJ48Av4JC";
+
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Copyright { get; set; }
+        [JsonProperty("media_type")]
+        public string MediaType { get; set; }
+
+        public static Task<Image> RequestApod()
+        {
+            var client = new RestClient(Address);
+            var request = new RestRequest(Method.GET);
+            var taskCompletionSource = new TaskCompletionSource<Image>();
+            var asyncHandle = client.ExecuteAsync<Image>(request, response =>
+            {
+                taskCompletionSource.SetResult(response.Data);
+            });
+
+            return taskCompletionSource.Task;
+        }
+    }
+}
